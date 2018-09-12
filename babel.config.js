1c511ef4115fc810da78557ea4a0f43323dc7140
Kara Todd <kara@voxel-fox.com>
2018-09-12 00:10:20 -0400

    update all the things

--- babel.config.js
+++ babel.config.js
@@ -1,31 +1,16 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {
-      loose: true,
-      modules: false,
-      useBuiltIns: 'usage',
-      shippedProposals: true,
-      targets: {
-        browsers: ['>0.25%', 'not dead']
-      }
-    }],
+    ['@babel/preset-env'],
     ['@babel/preset-react', {
       useBuiltIns: true,
       pragma: 'React.createElement'
     }]
   ],
   plugins: [
-    ['@babel/plugin-proposal-class-properties', {
-      loose: true
-    }],
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-syntax-dynamic-import',
     'babel-plugin-macros',
-    ['@babel/plugin-proposal-object-rest-spread', {
-      'useBuiltIns': true
-    }],
-    ['@babel/plugin-transform-runtime', {
-      helpers: true,
-      regenerator: true
-    }]
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-transform-runtime'
   ]
 }
