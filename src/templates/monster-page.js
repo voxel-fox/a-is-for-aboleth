050bcb63072ff5bf5c001433aab1cb7fd9a445d3
Kara Todd <kara@voxel-fox.com>
2019-01-06 20:06:38 -0500

    add some SEO stuff

--- src/templates/monster-page.js
+++ src/templates/monster-page.js
@@ -1,47 +1,48 @@
-import * as PropTypes from 'prop-types'
-import React from 'react'
-import { graphql } from 'gatsby'
-import MonsterDetail from '../components/monster-detail'
+import * as PropTypes from "prop-types";
+import React from "react";
+import { graphql } from "gatsby";
+import Helmet from "react-helmet";
+
+import MonsterDetail from "../components/monster-detail";
 
 class MonsterTemplate extends React.Component {
   static propTypes = {
     data: PropTypes.shape({
       monster: PropTypes.object.isRequired
     })
-  }
+  };
 
-  render () {
-    const {
-      monster,
-      imageFallback,
-      imageMonster
-    } = this.props.data
+  render() {
+    const { monster, imageFallback, imageMonster } = this.props.data;
 
     return (
-      <MonsterDetail
-        monster={monster}
-        image={(imageMonster && imageMonster.sharp) || (imageFallback && imageFallback.sharp)}
-      />
-    )
+      <>
+        <Helmet title={`${monster.name} (5e)`}>
+          <meta name="description" content={`Stats, actions and details for the ${monster.name} a ${monster.cr} CR creature. This monster is typically of a ${monster.alignment} alignment and is ${monster.size} sized. It has ${monster.actions.length} actions.`} />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+        </Helmet>
+        <MonsterDetail monster={monster} image={(imageMonster && imageMonster.sharp) || (imageFallback && imageFallback.sharp)} />
+      </>
+    );
   }
 }
 
-export default MonsterTemplate
+export default MonsterTemplate;
 
 export const pageQuery = graphql`
   query($name: String!, $imgRegexType: String!, $imgRegexMonster: String!) {
     monster: monstersSrd5EJson(name: { eq: $name }) {
       ...MonsterFields
     }
-    imageFallback: file(relativePath: {regex: $imgRegexType }) {
+    imageFallback: file(relativePath: { regex: $imgRegexType }) {
       sharp: childImageSharp {
         ...MonsterMast_img
       }
     }
-    imageMonster: file(relativePath: {regex: $imgRegexMonster }) {
+    imageMonster: file(relativePath: { regex: $imgRegexMonster }) {
       sharp: childImageSharp {
         ...MonsterMast_img
       }
     }
   }
-`
+`;
